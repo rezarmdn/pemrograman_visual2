fac5ec964f5dd04a72c13f50dd45cd6c96f03001
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2023-04-04 23:40:02 +0700

    Add files via upload

---
+++ Pertemuan 3/Latihan 3/Layang_Layang/Layang_Layang/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Layang_Layang
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub b_Hitung_Click(sender As Object, e As EventArgs) Handles b_Hitung.Click
+        Dim Diagonal1, Diagonal2, Sisi1, Sisi2, Luas, Keliling As Integer
+
+        Diagonal1 = Val(TextDiagonal1.Text)
+        Diagonal2 = Val(TextDiagonal2.Text)
+        Sisi1 = Val(TextSisi1.Text)
+        Sisi2 = Val(TextSisi2.Text)
+
+        Luas = 0.5 * (Diagonal1 * Diagonal2)
+        Keliling = 2 * (Sisi1 + Sisi2)
+
+        TextLuas.Text = Str(Luas)
+        TextKeliling.Text = Str(Keliling)
+
+
+    End Sub
+End Class
