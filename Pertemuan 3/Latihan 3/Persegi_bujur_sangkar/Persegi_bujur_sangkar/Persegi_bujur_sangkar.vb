5d3eae9a9ef8f98eae096c25cb7c868aa349f555
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2023-04-04 23:40:55 +0700

    Add files via upload

---
+++ Pertemuan 3/Latihan 3/Persegi_bujur_sangkar/Persegi_bujur_sangkar/Persegi_bujur_sangkar.vb
@@ -0,0 +1,14 @@
+﻿Public Class Persegi_bujur_sangkar
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Panjang, Luas, Keliling As Integer
+
+        Panjang = Val(TextPanjang.Text)
+
+        Luas = Panjang * Panjang
+        Keliling = 4 * Panjang
+
+        TextLuas.Text = Str(Luas)
+        TextKeliling.Text = Str(Keliling)
+
+    End Sub
+End Class
