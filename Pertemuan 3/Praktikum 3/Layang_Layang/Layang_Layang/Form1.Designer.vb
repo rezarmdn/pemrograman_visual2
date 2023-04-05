89f7a4e5fdb75765a4ef52af3eee18728a925cf0
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2023-04-06 04:37:20 +0700

    Add files via upload

---
+++ Pertemuan 3/Praktikum 3/Layang_Layang/Layang_Layang/Form1.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Layang_Layang
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextDiagonal1 = New System.Windows.Forms.TextBox()
+        Me.TextDiagonal2 = New System.Windows.Forms.TextBox()
+        Me.TextSisi1 = New System.Windows.Forms.TextBox()
+        Me.TextSisi2 = New System.Windows.Forms.TextBox()
+        Me.TextLuas = New System.Windows.Forms.TextBox()
+        Me.TextKeliling = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.b_Hitung = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(188, 89)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(142, 22)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "layang_layang"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(113, 145)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(109, 22)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Diagonal 1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(113, 183)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(109, 22)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Diagonal 2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(113, 221)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 22)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Sisi 1"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(113, 252)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(59, 22)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Sisi 2"
+        '
+        'TextDiagonal1
+        '
+        Me.TextDiagonal1.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextDiagonal1.Location = New System.Drawing.Point(256, 142)
+        Me.TextDiagonal1.Name = "TextDiagonal1"
+        Me.TextDiagonal1.Size = New System.Drawing.Size(149, 30)
+        Me.TextDiagonal1.TabIndex = 5
+        '
+        'TextDiagonal2
+        '
+        Me.TextDiagonal2.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextDiagonal2.Location = New System.Drawing.Point(256, 180)
+        Me.TextDiagonal2.Name = "TextDiagonal2"
+        Me.TextDiagonal2.Size = New System.Drawing.Size(149, 30)
+        Me.TextDiagonal2.TabIndex = 6
+        '
+        'TextSisi1
+        '
+        Me.TextSisi1.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextSisi1.Location = New System.Drawing.Point(256, 218)
+        Me.TextSisi1.Name = "TextSisi1"
+        Me.TextSisi1.Size = New System.Drawing.Size(149, 30)
+        Me.TextSisi1.TabIndex = 7
+        '
+        'TextSisi2
+        '
+        Me.TextSisi2.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextSisi2.Location = New System.Drawing.Point(256, 254)
+        Me.TextSisi2.Name = "TextSisi2"
+        Me.TextSisi2.Size = New System.Drawing.Size(149, 30)
+        Me.TextSisi2.TabIndex = 8
+        '
+        'TextLuas
+        '
+        Me.TextLuas.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextLuas.Location = New System.Drawing.Point(256, 354)
+        Me.TextLuas.Name = "TextLuas"
+        Me.TextLuas.Size = New System.Drawing.Size(149, 30)
+        Me.TextLuas.TabIndex = 9
+        '
+        'TextKeliling
+        '
+        Me.TextKeliling.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextKeliling.Location = New System.Drawing.Point(256, 390)
+        Me.TextKeliling.Name = "TextKeliling"
+        Me.TextKeliling.Size = New System.Drawing.Size(149, 30)
+        Me.TextKeliling.TabIndex = 10
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(113, 357)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(57, 22)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Luas"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(111, 393)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(83, 22)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Keliling"
+        '
+        'b_Hitung
+        '
+        Me.b_Hitung.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b_Hitung.Location = New System.Drawing.Point(256, 299)
+        Me.b_Hitung.Name = "b_Hitung"
+        Me.b_Hitung.Size = New System.Drawing.Size(149, 38)
+        Me.b_Hitung.TabIndex = 13
+        Me.b_Hitung.Text = "Hitung"
+        Me.b_Hitung.UseVisualStyleBackColor = True
+        '
+        'Layang_Layang
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(518, 556)
+        Me.Controls.Add(Me.b_Hitung)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextKeliling)
+        Me.Controls.Add(Me.TextLuas)
+        Me.Controls.Add(Me.TextSisi2)
+        Me.Controls.Add(Me.TextSisi1)
+        Me.Controls.Add(Me.TextDiagonal2)
+        Me.Controls.Add(Me.TextDiagonal1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Layang_Layang"
+        Me.Text = "Latihan 1 Layang-Layang"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextDiagonal1 As TextBox
+    Friend WithEvents TextDiagonal2 As TextBox
+    Friend WithEvents TextSisi1 As TextBox
+    Friend WithEvents TextSisi2 As TextBox
+    Friend WithEvents TextLuas As TextBox
+    Friend WithEvents TextKeliling As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents b_Hitung As Button
+End Class
---
+++ Pertemuan 3/Praktikum 3/Layang_Layang/Layang_Layang/Layang_Layang.vb
@@ -0,0 +1,34 @@
+﻿Public Class Layang_Layang
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Function HitungLuas(Diagonal1 As Integer, Diagonal2 As Integer, Sisi As Integer) As Integer
+        Dim Luas As Integer
+        Luas = 0.5 * (Diagonal1 * Diagonal2)
+        Return Luas
+    End Function
+
+    Private Function HitungKeliling(Diagonal1 As Integer, Diagonal2 As Integer, Sisi1 As Integer, Sisi2 As Integer) As Integer
+        Dim Keliling As Integer
+        Keliling = 2 * (Sisi1 + Sisi2)
+        Return Keliling
+    End Function
+
+    Private Sub b_Hitung_Click(sender As Object, e As EventArgs) Handles b_Hitung.Click
+        Dim Diagonal1, Diagonal2, Sisi1, Sisi2, Luas, Keliling As Integer
+
+        Diagonal1 = Val(TextDiagonal1.Text)
+        Diagonal2 = Val(TextDiagonal2.Text)
+        Sisi1 = Val(TextSisi1.Text)
+        Sisi2 = Val(TextSisi2.Text)
+
+        Luas = HitungLuas(Diagonal1, Diagonal2, Sisi1)
+        Keliling = HitungKeliling(Diagonal1, Diagonal2, Sisi1, Sisi2)
+
+        TextLuas.Text = Str(Luas)
+        TextKeliling.Text = Str(Keliling)
+
+
+    End Sub
+End Class
