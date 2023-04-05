5eff52cf081ab0f638212c404c76a038b398bf01
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2023-04-06 04:47:04 +0700

    Add files via upload

---
+++ Pertemuan 3/Praktikum 3/segitiga/segitiga/Segitiga.vb
@@ -0,0 +1,32 @@
+﻿Public Class Segitiga
+
+    Private Function HitungLuas(Alas As Integer, Tinggi As Integer) As Integer
+        Dim Luas As Integer
+        Luas = 0.5 * Alas * Tinggi
+        Return Luas
+    End Function
+
+    Private Function HitungKeliling(Sisi1 As Integer, Sisi2 As Integer, Sisi3 As Integer) As Integer
+        Dim Keliling As Integer
+        Keliling = Sisi1 + Sisi2 + Sisi3
+        Return Keliling
+    End Function
+
+    Private Sub B_Hitung_Click(sender As Object, e As EventArgs) Handles B_Hitung.Click
+        Dim Alas, Tinggi, Sisi1, Sisi2, Sisi3, Luas, Keliling As Integer
+
+        Alas = Val(TextAlas.Text)
+        Tinggi = Val(TextTinggi.Text)
+        Sisi1 = Val(TextSisi1.Text)
+        Sisi2 = Val(TextSisi2.Text)
+        Sisi3 = Val(TextSisi3.Text)
+
+        Luas = HitungLuas(Alas, Tinggi)
+        Keliling = HitungKeliling(Sisi1, Sisi2, Sisi3)
+
+        TextLuas.Text = Str(Luas)
+        TextKeliling.Text = Str(Keliling)
+
+
+    End Sub
+End Class
