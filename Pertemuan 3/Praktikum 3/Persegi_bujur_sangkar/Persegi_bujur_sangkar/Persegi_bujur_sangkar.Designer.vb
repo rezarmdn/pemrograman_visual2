3e801f0bb245edcbc9a1b0fe6a20449c31cc3879
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2023-04-06 04:42:04 +0700

    Add files via upload

---
+++ Pertemuan 3/Praktikum 3/Persegi_bujur_sangkar/Persegi_bujur_sangkar/Persegi_bujur_sangkar.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Persegi_bujur_sangkar
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextPanjang = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextLuas = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextKeliling = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("News706 BT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(160, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(245, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "persegi Bujur Sangkar"
+        '
+        'TextPanjang
+        '
+        Me.TextPanjang.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextPanjang.Location = New System.Drawing.Point(202, 149)
+        Me.TextPanjang.Name = "TextPanjang"
+        Me.TextPanjang.Size = New System.Drawing.Size(144, 30)
+        Me.TextPanjang.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("News706 BT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(226, 121)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "panjang"
+        '
+        'TextLuas
+        '
+        Me.TextLuas.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.TextLuas.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextLuas.Location = New System.Drawing.Point(202, 293)
+        Me.TextLuas.Name = "TextLuas"
+        Me.TextLuas.Size = New System.Drawing.Size(144, 30)
+        Me.TextLuas.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("News706 BT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label3.Location = New System.Drawing.Point(241, 265)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 25)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Luas"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(202, 207)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(144, 34)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Hitung"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("News706 BT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label4.Location = New System.Drawing.Point(226, 335)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(94, 25)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Keliling"
+        '
+        'TextKeliling
+        '
+        Me.TextKeliling.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.TextKeliling.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextKeliling.Location = New System.Drawing.Point(202, 363)
+        Me.TextKeliling.Name = "TextKeliling"
+        Me.TextKeliling.Size = New System.Drawing.Size(144, 30)
+        Me.TextKeliling.TabIndex = 6
+        '
+        'Persegi_bujur_sangkar
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(558, 463)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextKeliling)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextLuas)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextPanjang)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Persegi_bujur_sangkar"
+        Me.Text = "Praktikum  3 Persegi Bujur Sangkar"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextPanjang As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextLuas As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextKeliling As TextBox
+End Class
---
+++ Pertemuan 3/Praktikum 3/Persegi_bujur_sangkar/Persegi_bujur_sangkar/Persegi_bujur_sangkar.vb
@@ -0,0 +1,27 @@
+﻿Public Class Persegi_bujur_sangkar
+
+    Private Function HitungLuas(Panjang As Integer) As Integer
+        Dim Luas As Integer
+        Luas = Panjang * Panjang
+        Return Luas
+    End Function
+
+    Private Function HitungKeliling(Panjang As Integer) As Integer
+        Dim Keliling As Integer
+        Keliling = 4 * Panjang
+        Return Keliling
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Panjang, Luas, Keliling As Integer
+
+        Panjang = Val(TextPanjang.Text)
+
+        Luas = HitungLuas(Panjang)
+        Keliling = HitungKeliling(Panjang)
+
+        TextLuas.Text = Str(Luas)
+        TextKeliling.Text = Str(Keliling)
+
+    End Sub
+End Class
