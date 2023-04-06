b40ec3498c40ffa67f41a408aae3caeeafd2295b
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2023-04-06 21:16:43 +0700

    Add files via upload

---
+++ Pertemuan 4/latihan 4/Celcius ke F R K/Celcius ke F R K/Konversi_Celcius_ke_Fahrenheit_Reamur_kelvin.vb
@@ -0,0 +1,31 @@
+﻿Public Class Konversi_Celcius_ke_Fahrenheit_Reamur_kelvin
+    Private Function ToFahrenheit(suhu As Decimal) As Decimal
+        Dim F As Decimal
+        F = (9 / 5) * suhu + 32
+        Return F
+    End Function
+
+    Private Function ToReamur(suhu As Decimal) As Decimal
+        Dim R As Decimal
+        R = 4 / 5 * suhu
+        Return R
+    End Function
+
+    Private Function ToKelvin(suhu As Decimal) As Decimal
+        Dim K As Decimal
+        K = suhu + 273
+        Return K
+    End Function
+
+    Private Sub B_Konvert_Click(sender As Object, e As EventArgs) Handles B_Konvert.Click
+        Dim C, F, R, K As Decimal
+        C = CDec(TextCelcius.Text)
+        F = ToFahrenheit(C)
+        R = ToReamur(C)
+        K = ToKelvin(C)
+
+        TextFahrenheit.Text = Str(F)
+        TextReamur.Text = Str(R)
+        TextKelvin.Text = Str(K)
+    End Sub
+End Class
---
+++ Pertemuan 4/latihan 4/Celcius ke F R K/Celcius ke F R K/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Celcius ke F R K")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Celcius ke F R K")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5eac4853-e2c3-4bc9-9248-f496d852cd72")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
