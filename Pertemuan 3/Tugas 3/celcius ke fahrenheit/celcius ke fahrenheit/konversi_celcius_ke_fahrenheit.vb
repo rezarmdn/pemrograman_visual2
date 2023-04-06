4255e63387b6d68f4ba3555b275bf324f4173a1c
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2023-04-06 10:17:33 +0700

    Add files via upload

---
+++ Pertemuan 3/Tugas 3/celcius ke fahrenheit/celcius ke fahrenheit/konversi_celcius_ke_fahrenheit.vb
@@ -0,0 +1,20 @@
+﻿Public Class konversi_celcius_ke_fahrenheit
+
+    Private Function HitungKonversiCelciusKeFahrenheit(Celcius As Integer) As Integer
+        Dim Fahrenheit As Integer
+        Fahrenheit = (Celcius * 9 / 5) + 32
+        Return Fahrenheit
+    End Function
+
+
+    Private Sub B_Hitung_Click(sender As Object, e As EventArgs) Handles B_Hitung.Click
+        Dim Celcius, CelciusFarenheit As Integer
+
+        Celcius = Val(TextCelciusFahrenheit.Text)
+
+        CelciusFarenheit = HitungKonversiCelciusKeFahrenheit(Celcius)
+
+        TextKonversiFahrenheit.Text = Str(CelciusFarenheit)
+
+    End Sub
+End Class
