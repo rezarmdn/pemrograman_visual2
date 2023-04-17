5eda58ba2fced0f9b0aa35e4f0387a48eb5c9876
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2023-04-17 21:05:57 +0700

    Add files via upload

---
+++ Pertemuan 5/Praktikum 5/Praktikum5_penggunaan_IF/Prkatikum5_Pengguna_if_2/Praktikum_Beri_Bintang_Gojek_By_Reza_Ramadhan.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Praktikum_Beri_Bintang_Gojek_By_Reza_Ramadhan
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtKeterangan = New System.Windows.Forms.TextBox()
+        Me.B_Submit = New System.Windows.Forms.Button()
+        Me.txtBeriBintang = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("News706 BT", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label5.Location = New System.Drawing.Point(203, 98)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(92, 32)
+        Me.Label5.TabIndex = 22
+        Me.Label5.Text = "Gojek"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(28, 397)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(435, 22)
+        Me.Label4.TabIndex = 21
+        Me.Label4.Text = "Creat By Reza Ramadhan - 200511078 - TI20 D"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label3.Location = New System.Drawing.Point(59, 267)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(325, 22)
+        Me.Label3.TabIndex = 20
+        Me.Label3.Text = "Keterangan Kepuasan Konsumen"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(59, 187)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(130, 22)
+        Me.Label2.TabIndex = 19
+        Me.Label2.Text = "Beri Bintang"
+        '
+        'txtKeterangan
+        '
+        Me.txtKeterangan.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtKeterangan.Location = New System.Drawing.Point(63, 292)
+        Me.txtKeterangan.Name = "txtKeterangan"
+        Me.txtKeterangan.Size = New System.Drawing.Size(375, 30)
+        Me.txtKeterangan.TabIndex = 18
+        '
+        'B_Submit
+        '
+        Me.B_Submit.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.B_Submit.Location = New System.Drawing.Point(225, 202)
+        Me.B_Submit.Name = "B_Submit"
+        Me.B_Submit.Size = New System.Drawing.Size(140, 48)
+        Me.B_Submit.TabIndex = 17
+        Me.B_Submit.Text = "Submit"
+        Me.B_Submit.UseVisualStyleBackColor = False
+        '
+        'txtBeriBintang
+        '
+        Me.txtBeriBintang.Location = New System.Drawing.Point(63, 212)
+        Me.txtBeriBintang.Name = "txtBeriBintang"
+        Me.txtBeriBintang.Size = New System.Drawing.Size(168, 30)
+        Me.txtBeriBintang.TabIndex = 16
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("News706 BT", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(173, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(177, 32)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "Praktikum 5"
+        '
+        'Praktikum_Beri_Bintang_Gojek_By_Reza_Ramadhan
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 22.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(491, 453)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtKeterangan)
+        Me.Controls.Add(Me.B_Submit)
+        Me.Controls.Add(Me.txtBeriBintang)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.Name = "Praktikum_Beri_Bintang_Gojek_By_Reza_Ramadhan"
+        Me.Text = "Praktikum_Beri_Bintang_Gojek_By_Reza_Ramadhan.vb"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtKeterangan As TextBox
+    Friend WithEvents B_Submit As Button
+    Friend WithEvents txtBeriBintang As TextBox
+    Friend WithEvents Label1 As Label
+End Class
