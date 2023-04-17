661092ea99966cf678c204b91b09e3cfeb13dcd3
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2023-04-17 21:04:09 +0700

    Add files via upload

---
+++ Pertemuan 5/Praktikum 5/Prkatikum5_Pengguna_if_2/Praktikum_Beri_Bintang_Gojek_By_Reza_Ramadhan.vb
@@ -0,0 +1,19 @@
+﻿Public Class Praktikum_Beri_Bintang_Gojek_By_Reza_Ramadhan
+    Private Sub B_Submit_Click(sender As Object, e As EventArgs) Handles B_Submit.Click
+        If (Val(txtBeriBintang.Text) >= 5) Then
+            txtKeterangan.Text = "sangat Puas"
+
+        ElseIf (txtBeriBintang.Text >= 4) Then
+            txtKeterangan.Text = "Puas"
+
+        ElseIf (txtBeriBintang.Text >= 3) Then
+            txtKeterangan.Text = "Biasa aja"
+
+        ElseIf (txtBeriBintang.Text >= 2) Then
+            txtKeterangan.Text = "Gak Puas"
+
+        Else
+            txtKeterangan.Text = "Sangat Gak Puas"
+        End If
+    End Sub
+End Class
