d95544892e8e2a52ae7bb9b99ddd0334575bb441
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2023-04-17 21:03:06 +0700

    Add files via upload

---
+++ Pertemuan 5/Praktikum 5/Praktikum5_penggunaan_IF/Praktikum5_penggunaan_IF/Praktikum_Berat_Badan_Ideal_By_Reza_Ramadhan.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtBeratBadan = New System.Windows.Forms.TextBox()
+        Me.B_Submit = New System.Windows.Forms.Button()
+        Me.txtKeterangan = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("News706 BT", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(200, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(177, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Praktikum 5"
+        '
+        'txtBeratBadan
+        '
+        Me.txtBeratBadan.Location = New System.Drawing.Point(94, 131)
+        Me.txtBeratBadan.Name = "txtBeratBadan"
+        Me.txtBeratBadan.Size = New System.Drawing.Size(168, 30)
+        Me.txtBeratBadan.TabIndex = 1
+        '
+        'B_Submit
+        '
+        Me.B_Submit.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.B_Submit.Location = New System.Drawing.Point(256, 121)
+        Me.B_Submit.Name = "B_Submit"
+        Me.B_Submit.Size = New System.Drawing.Size(140, 48)
+        Me.B_Submit.TabIndex = 2
+        Me.B_Submit.Text = "Submit"
+        Me.B_Submit.UseVisualStyleBackColor = False
+        '
+        'txtKeterangan
+        '
+        Me.txtKeterangan.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtKeterangan.Location = New System.Drawing.Point(94, 211)
+        Me.txtKeterangan.Name = "txtKeterangan"
+        Me.txtKeterangan.Size = New System.Drawing.Size(375, 30)
+        Me.txtKeterangan.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(90, 106)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(129, 22)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Berat Badan"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label3.Location = New System.Drawing.Point(90, 186)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(244, 22)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Keterangan Berat Badan"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(59, 316)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(435, 22)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Creat By Reza Ramadhan - 200511078 - TI20 D"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 22.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(553, 359)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtKeterangan)
+        Me.Controls.Add(Me.B_Submit)
+        Me.Controls.Add(Me.txtBeratBadan)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.Name = "Form1"
+        Me.Text = "Reza Ramadhan - 200511078 - TI  20 D"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtBeratBadan As TextBox
+    Friend WithEvents B_Submit As Button
+    Friend WithEvents txtKeterangan As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Pertemuan 5/Praktikum 5/Praktikum5_penggunaan_IF/Praktikum5_penggunaan_IF/Praktikum_Berat_Badan_Ideal_By_Reza_Ramadhan.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub B_Submit_Click(sender As Object, e As EventArgs) Handles B_Submit.Click
+        If (Val(txtBeratBadan.Text) >= 75) Then
+            txtKeterangan.Text = "Mending Diet AJa Berat Badan Udah Terlalu Besar"
+
+        ElseIf (txtBeratBadan.Text >= 67) Then
+            txtKeterangan.Text = "Berat Badan Besar"
+
+        ElseIf (txtBeratBadan.Text >= 62) Then
+            txtKeterangan.Text = "Berat Badan Sedang"
+
+        ElseIf (txtBeratBadan.Text >= 57) Then
+            txtKeterangan.Text = "Berat Badan Kecil"
+
+        Else
+            txtKeterangan.Text = "Maaf Berat Badan Terlalu Kecil"
+        End If
+    End Sub
+End Class
