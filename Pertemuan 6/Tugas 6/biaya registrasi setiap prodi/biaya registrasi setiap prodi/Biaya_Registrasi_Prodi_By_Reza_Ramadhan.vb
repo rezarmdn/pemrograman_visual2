a9d92b45ebf9e94bd1038c70d84699010bdd9841
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2023-04-30 09:02:33 +0700

    Add files via upload

---
+++ Pertemuan 6/Tugas 6/biaya registrasi setiap prodi/biaya registrasi setiap prodi/Biaya_Registrasi_Prodi_By_Reza_Ramadhan.vb
@@ -0,0 +1,28 @@
+﻿Public Class Biaya_Registrasi_Prodi_By_Reza_Ramadhan
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbProdi.Items.Clear()
+        cmbProdi.Items.Add("Manajemen")
+        cmbProdi.Items.Add("Akuntansi")
+        cmbProdi.Items.Add("Ilmu Komunikasi")
+        cmbProdi.Items.Add("Peternakan")
+        cmbProdi.Items.Add("Teknik Industri")
+        cmbProdi.Items.Add("Teknik Informatika")
+    End Sub
+    Private Sub cmbProdi_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbProdi.SelectedIndexChanged
+        If cmbProdi.Text = "Manajemen" Then
+            txtRegistrasi.Text = "Rp 4.500.000,-"
+        ElseIf cmbProdi.Text = "Akuntansi" Then
+            txtRegistrasi.Text = "Rp 5.800.000,-"
+        ElseIf cmbProdi.Text = "Ilmu Komunikasi" Then
+            txtRegistrasi.Text = "Rp 6.800.000,-"
+        ElseIf cmbProdi.Text = "Peternakan" Then
+            txtRegistrasi.Text = "Rp 7.800.000,-"
+        ElseIf cmbProdi.Text = "Teknik Industri" Then
+            txtRegistrasi.Text = "Rp 8.000.000,-"
+        ElseIf cmbProdi.Text = "Teknik Informatika" Then
+            txtRegistrasi.Text = "Rp 9.800.000,-"
+        End If
+
+    End Sub
+
+End Class
---
+++ Pertemuan 6/Tugas 6/biaya registrasi setiap prodi/biaya registrasi setiap prodi/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("biaya registrasi setiap prodi")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("biaya registrasi setiap prodi")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ceae8d9d-0cbb-4958-b10b-cfb26d4d485f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
