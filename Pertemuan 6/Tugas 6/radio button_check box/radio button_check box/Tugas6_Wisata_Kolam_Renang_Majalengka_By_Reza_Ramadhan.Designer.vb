f54597787bc959d5bb2f1b5ac6a12dc2a00b2220
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2023-05-04 18:16:56 +0700

    Add files via upload

---
+++ Pertemuan 6/Tugas 6/radio button_check box/radio button_check box/Tugas6_Wisata_Kolam_Renang_Majalengka_By_Reza_Ramadhan.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Tugas6_Wisata_Kolam_Renang_Majalengka_By_Reza_Ramadhan
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.rbanak = New System.Windows.Forms.RadioButton()
+        Me.rbdewasa = New System.Windows.Forms.RadioButton()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.cbnonmember = New System.Windows.Forms.CheckBox()
+        Me.cbmember = New System.Windows.Forms.CheckBox()
+        Me.btnhitung = New System.Windows.Forms.Button()
+        Me.txttotal = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("News706 BT", 18.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.Location = New System.Drawing.Point(237, 44)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(318, 29)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Aplikasi Pemesanan Tiket"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("News706 BT", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(197, 92)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(417, 29)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Wisata Kolam Renang Majalengka"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(74, 215)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(189, 22)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Harga Tiket Masuk"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.Label4.Location = New System.Drawing.Point(75, 282)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(200, 22)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Anak - anak = 15.000"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.Label5.Location = New System.Drawing.Point(75, 339)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(165, 22)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Dewasa = 20.000"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.Label6.Location = New System.Drawing.Point(322, 215)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(126, 22)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Tiket Masuk"
+        '
+        'rbanak
+        '
+        Me.rbanak.AutoSize = True
+        Me.rbanak.Location = New System.Drawing.Point(326, 282)
+        Me.rbanak.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+        Me.rbanak.Name = "rbanak"
+        Me.rbanak.Size = New System.Drawing.Size(140, 26)
+        Me.rbanak.TabIndex = 6
+        Me.rbanak.TabStop = True
+        Me.rbanak.Text = "Anak - Anak"
+        Me.rbanak.UseVisualStyleBackColor = True
+        '
+        'rbdewasa
+        '
+        Me.rbdewasa.AutoSize = True
+        Me.rbdewasa.Location = New System.Drawing.Point(326, 337)
+        Me.rbdewasa.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+        Me.rbdewasa.Name = "rbdewasa"
+        Me.rbdewasa.Size = New System.Drawing.Size(102, 26)
+        Me.rbdewasa.TabIndex = 7
+        Me.rbdewasa.TabStop = True
+        Me.rbdewasa.Text = "Dewasa"
+        Me.rbdewasa.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.Label7.Location = New System.Drawing.Point(542, 214)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(90, 22)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "Member"
+        '
+        'cbnonmember
+        '
+        Me.cbnonmember.AutoSize = True
+        Me.cbnonmember.Location = New System.Drawing.Point(548, 278)
+        Me.cbnonmember.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+        Me.cbnonmember.Name = "cbnonmember"
+        Me.cbnonmember.Size = New System.Drawing.Size(152, 26)
+        Me.cbnonmember.TabIndex = 9
+        Me.cbnonmember.Text = "Non Member"
+        Me.cbnonmember.UseVisualStyleBackColor = True
+        '
+        'cbmember
+        '
+        Me.cbmember.AutoSize = True
+        Me.cbmember.Location = New System.Drawing.Point(546, 340)
+        Me.cbmember.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+        Me.cbmember.Name = "cbmember"
+        Me.cbmember.Size = New System.Drawing.Size(213, 26)
+        Me.cbmember.TabIndex = 10
+        Me.cbmember.Text = "Member (Disc 10%)"
+        Me.cbmember.UseVisualStyleBackColor = True
+        '
+        'btnhitung
+        '
+        Me.btnhitung.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btnhitung.Location = New System.Drawing.Point(283, 395)
+        Me.btnhitung.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+        Me.btnhitung.Name = "btnhitung"
+        Me.btnhitung.Size = New System.Drawing.Size(205, 50)
+        Me.btnhitung.TabIndex = 11
+        Me.btnhitung.Text = "Hitung"
+        Me.btnhitung.UseVisualStyleBackColor = True
+        '
+        'txttotal
+        '
+        Me.txttotal.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txttotal.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txttotal.Location = New System.Drawing.Point(283, 501)
+        Me.txttotal.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+        Me.txttotal.Name = "txttotal"
+        Me.txttotal.Size = New System.Drawing.Size(205, 30)
+        Me.txttotal.TabIndex = 12
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.Label8.Location = New System.Drawing.Point(322, 464)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(121, 22)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "Total Harga"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("News706 BT", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.ForeColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Label9.Location = New System.Drawing.Point(129, 598)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(556, 29)
+        Me.Label9.TabIndex = 14
+        Me.Label9.Text = "Creat By Reza Ramadhan - 200511078 - TI 20 D"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 22.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(789, 636)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txttotal)
+        Me.Controls.Add(Me.btnhitung)
+        Me.Controls.Add(Me.cbmember)
+        Me.Controls.Add(Me.cbnonmember)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.rbdewasa)
+        Me.Controls.Add(Me.rbanak)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("News706 BT", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+        Me.Name = "Form1"
+        Me.Text = "Reza Ramadhan - 200511078 - TI 20 D"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents rbanak As RadioButton
+    Friend WithEvents rbdewasa As RadioButton
+    Friend WithEvents Label7 As Label
+    Friend WithEvents cbnonmember As CheckBox
+    Friend WithEvents cbmember As CheckBox
+    Friend WithEvents btnhitung As Button
+    Friend WithEvents txttotal As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+End Class
---
+++ Pertemuan 6/Tugas 6/radio button_check box/radio button_check box/Tugas6_Wisata_Kolam_Renang_Majalengka_By_Reza_Ramadhan.vb
@@ -0,0 +1,29 @@
+﻿Public Class Tugas6_Wisata_Kolam_Renang_Majalengka_By_Reza_Ramadhan
+    Private Sub btnhitung_Click(sender As Object, e As EventArgs) Handles btnhitung.Click
+        Dim TiketMasuk As Decimal
+        Dim Member As Decimal
+        Dim Total As Decimal
+
+        If rbanak.Checked = True Then
+            TiketMasuk = 15000
+        ElseIf rbdewasa.Checked = True Then
+            TiketMasuk = 20000
+        End If
+
+        If cbnonmember.Checked = True Then
+            Member = 0
+        End If
+
+        If cbmember.Checked = True Then
+            Member = (TiketMasuk * 10 / 100)
+        End If
+
+        Total = TiketMasuk - Member
+
+        txttotal.Text = Total
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+End Class
