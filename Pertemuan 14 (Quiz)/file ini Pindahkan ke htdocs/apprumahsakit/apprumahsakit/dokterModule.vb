d1f9aa63d2307856c42377f383cbe92fed79dbaa
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2023-07-13 14:03:09 +0700

    Add files via upload

---
+++ Pertemuan 14 (Quiz)/file ini Pindahkan ke htdocs/apprumahsakit/apprumahsakit/dokterModule.vb
@@ -0,0 +1,7 @@
+﻿Module dokterModule
+
+    Public api_folder As String = "appsrumahsakit_Reza"
+    Public dokter_api As String = "http://localhost/" & api_folder & "/dokter_api.php"
+    Public dokter_baru As Boolean
+
+End Module
